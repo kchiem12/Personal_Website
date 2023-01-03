a9a4c923a29f9e2413d59c976038919f3c7189f5
ken <kchiem12@gmail.com>
2023-01-02 19:23:34 -0500

    complete website w/o profile pic

---
+++ js/main.js
@@ -0,0 +1,117 @@
+const dropdownHeaders = document.querySelectorAll('.dropdown-label');
+
+// for (i = 0; i < dropdown.length; i++) {
+//     dropdown[i].addEventListener('click', () => {
+//         this.classList.toggle('active');
+//     })
+// }
+
+dropdownHeaders.forEach(header => {
+    header.addEventListener('click', e => {
+        header.classList.toggle('active');
+        const dropdownBody = header.nextElementSibling;
+
+        if (header.classList.contains('active')) {
+            dropdownBody.style.maxHeight = dropdownBody.scrollHeight + 'px';
+        } else {
+            dropdownBody.style.maxHeight = 0;
+        }
+    });
+})
+
+// for enlarging images
+
+const modalImages = document.querySelector('.modal-imgs');
+const modalVideos = document.querySelector('.modal-vids');
+const previewsStatic = document.querySelectorAll('.previews img');
+const previewVideo = document.querySelectorAll('.previews video');
+const fullImage = document.querySelector('.full-img');
+const fullVideo = document.getElementById('fullvideo');
+const imgText = document.querySelector('.caption');
+
+previewVideo.forEach(preview => {
+    preview.load();
+});
+
+previewsStatic.forEach(preview => {
+    preview.addEventListener('click', () => {
+        modalImages.classList.add('open');
+        fullImage.classList.add('open');
+        const image = preview.getAttribute('src');
+
+
+        fullImage.src = `${image}`;
+    });
+});
+
+previewVideo.forEach(preview => {
+    preview.addEventListener('click', () => {
+        modalVideos.classList.add('open');
+        const video = preview.firstElementChild.getAttribute('src');
+
+        fullVideo.firstElementChild.setAttribute('src', "." + video);
+        fullVideo.load();
+    });
+});
+
+modalImages.addEventListener('click', (e) => {
+    if(e.target.classList.contains('modals')) {
+        modalImages.classList.remove('open');
+        fullImage.classList.remove('open');
+        fullImage.src = '';
+    };
+});
+
+modalVideos.addEventListener('click', (e) => {
+    if (e.target.classList.contains('modals')) {
+        modalVideos.classList.remove('open');
+        fullVideo.firstElementChild.setAttribute('src', '');
+    };
+});
+
+//Handles navigation bar transition
+const navBar = document.querySelector('.navbar');
+const mobileHamburger = document.querySelector('.mobile-menu');
+const navbarLinks= document.querySelector('.buttons');
+
+mobileHamburger.addEventListener('click', () => {
+    navBar.classList.toggle('menu-open');
+    mobileHamburger.classList.toggle('is-active');
+    navbarLinks.classList.toggle('active');
+});
+
+// const menuNav = document.querySelector('.navbar-container');
+// const menuLinks = document.querySelectorAll('.nav-links');
+
+// document.onreadystatechange = function() {
+//     let lScrollPosition = 0;
+//     window.addEventListener('scroll', (e) => {
+
+//         lScrollPosition = window.scrollY;
+
+//         if (lScrollPosition> 1) {
+//             menuNav.classList.add('scrolled-navbar');
+//             for (const links of menuLinks) {
+//                 links.classList.add('scrolled-navbar');
+//             }
+//         } else {
+//             menuNav.classList.remove('scrolled-navbar');
+//             for (const links of menuLinks) {
+//                 links.classList.remove('scrolled-navbar');
+//             }
+//         }
+//     });
+// };
+
+// Below is to hide the navbar when user scrolls down, and let it reappear
+// when scrolled upwards
+var lastScroll = window.scrollY;
+
+window.addEventListener('scroll', () => {
+    if (lastScroll >= window.scrollY || navBar.classList.contains('menu-open') || window.scrollY <= 70) {
+        document.getElementById('navbar').style.top = "0";
+    } else {
+        document.getElementById('navbar').style.top = "-100px";
+    }
+    lastScroll = window.scrollY;
+})
